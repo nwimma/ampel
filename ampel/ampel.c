884e4b5a7f14f126d88b9e04fd1dfe9d41f153b2
Florian Hamacsek <fhamacsek@student.tgm.ac.at>
2021-12-21 12:51:26 +0100

    Build Folder, Ampel Folder

---
+++ ampel/ampel.c
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "pico/stdlib.h"
+#define SLEEP_TIME 4000
+void setupPin(uint);
+
+int main() {
+	stdio_init_all();
+	const uint ONBOARD_PIN = PICO_DEFAULT_LED_PIN;
+	setupPin(ONBOARD_PIN);
+	gpio_put(ONBOARD_PIN, 1);
+
+	const uint GREEN_PIN = 9;
+	const uint YELLOW_PIN = 11;
+	const uint RED_PIN = 13;
+
+	setupPin(GREEN_PIN);
+	setupPin(YELLOW_PIN);
+	setupPin(RED_PIN);
+
+	while (1) {
+		gpio_put(RED_PIN, 1);
+		sleep_ms(SLEEP_TIME);
+		gpio_put(RED_PIN, 0);
+
+		gpio_put(YELLOW_PIN, 1);
+		sleep_ms(SLEEP_TIME / 2);
+		gpio_put(YELLOW_PIN, 0);
+
+		gpio_put(GREEN_PIN, 1);
+		sleep_ms(SLEEP_TIME);
+		gpio_put(GREEN_PIN, 0);
+
+		for(int i = 0; i < 4; i++) {
+			sleep_ms(SLEEP_TIME / 4);
+			gpio_put(GREEN_PIN, 1);
+			sleep_ms(SLEEP_TIME / 4);
+			gpio_put(GREEN_PIN, 0);
+		}
+
+		gpio_put(YELLOW_PIN, 1);
+		sleep_ms(SLEEP_TIME / 2);
+		gpio_put(YELLOW_PIN, 0);
+	}
+}
+
+void setupPin(uint pin) {
+	gpio_init(pin);
+	gpio_set_dir(pin, GPIO_OUT);
+}
